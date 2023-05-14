a7cec6fb00c2c7c5ca8dd72b5c52d3bf51e3d725
David Mukajanga <mukajangadavid@gmail.com>
2023-05-14 14:40:50 +0300

    This is an unintended commit.

--- resources/index.js/index.js
+++ resources/index.js/index.js
@@ -1,20 +1,5 @@
 let menuToggle = document.querySelector(".toggle");
 
-let parentNav = document.getElementById("main_nav");
-
 menuToggle.onclick = function() {
     menuToggle.classList.toggle("active");
 }
-
-// if (menuToggle.onclick) {
-//         parentNav.style.cssText = `
-//         display: flex;
-//         position: fixed;
-//         z-index: 1;
-//         width: 100%;
-//         height: 100%;
-//         flex-direction: column;
-//         justify-content: center;
-//         align-items: center;
-//         `;
-// }
