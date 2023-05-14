dfc310e522c1071f04b27d39a6927d510c966f27
David Mukajanga <mukajangadavid@gmail.com>
2023-05-14 09:29:42 +0300

    Corrected the mistake with the picture.

--- resources/index.js/index.js
+++ resources/index.js/index.js
@@ -1,5 +1,20 @@
-let menutoggle = document.querySelector(".toggle");
+let menuToggle = document.querySelector(".toggle");
 
-menutoggle.onclick = function() {
-    menutoggle.classList.toggle("active")
+let parentNav = document.getElementById("main_nav");
+
+menuToggle.onclick = function() {
+    menuToggle.classList.toggle("active");
 }
+
+// if (menuToggle.onclick) {
+//         parentNav.style.cssText = `
+//         display: flex;
+//         position: fixed;
+//         z-index: 1;
+//         width: 100%;
+//         height: 100%;
+//         flex-direction: column;
+//         justify-content: center;
+//         align-items: center;
+//         `;
+// }
