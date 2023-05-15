c00703e20f3349a209668384586e35b6ce5f1351
David Mukajanga <mukajangadavid@gmail.com>
2023-05-15 20:54:06 +0300

    Put the nav bar on the CSS page

---
+++ Instructional htmls/02._CSS/introduction/resources/js/index.js
@@ -0,0 +1,16 @@
+let menuToggle = document.querySelector(".toggle");
+
+menuToggle.onclick = function() {
+    menuToggle.classList.toggle("active");
+};
+
+
+// var body = document.getElementsByTagName("body");
+
+// var mainHeading = document.getElementById("main-heading");
+
+// var secHeading = document.querySelector(".secHeading");
+
+// body.onload = function() {
+//     mainHeading.innerHTML = secHeading
+// };
