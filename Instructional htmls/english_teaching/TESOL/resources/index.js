ce139264c650637705f999d3462d8345915cd01e
David Mukajanga <mukajangadavid@gmail.com>
2023-08-02 15:16:20 +0300

    Beautified English-teaching pages and put the top-nav on them.

---
+++ Instructional htmls/english_teaching/TESOL/resources/index.js
@@ -0,0 +1,35 @@
+
+// Code for the menu button 
+let menuToggle = document.querySelector(".toggle");
+
+menuToggle.onclick = function() {
+    menuToggle.classList.toggle("active");
+}
+
+
+
+// Code For Accordion and Its Animations
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+
+    // if (panel.style.display === "block") {
+    //   panel.style.display = "none";
+    // } else {
+    //   panel.style.display = "block";
+    // }
+  });
+}
